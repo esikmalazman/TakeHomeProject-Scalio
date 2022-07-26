4ccc984f22faed9e34a49832dbfe0b1958143852
Ikmal Azman <opikmalazman@gmail.com>
2022-07-26 19:50:44 +0800

    init starting project files

---
+++ GithubExplorerApp-TakeHomeProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubExplorerApp-TakeHomeProject
+//
+//  Created by Ikmal Azman on 26/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
