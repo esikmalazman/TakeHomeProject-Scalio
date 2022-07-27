fa0a7017a8ced79975bdf496c051061e1a530cd5
Ikmal Azman <opikmalazman@gmail.com>
2022-07-27 23:24:31 +0800

    create initial test suite for result view controller

---
+++ GithubExplorerApp-TakeHomeProjectTests/ResultsViewControllerTests.swift
@@ -0,0 +1,9 @@
+@testable import GithubExplorerApp_TakeHomeProject
+import XCTest
+
+final class ResultsViewControllerTests: XCTestCase {
+
+    func test_zero() throws {
+        XCTFail("Tests not yet implemented in ResultsViewControllerTests")
+    }
+}
