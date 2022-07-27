866f1b9d0d7d31ca8f909c31b22cb3872e43d31f
Ikmal Azman <opikmalazman@gmail.com>
2022-07-27 08:15:32 +0800

    create method to perform network request

--- GithubExplorerApp-TakeHomeProject/ViewModel/SearchResultViewModel.swift
+++ GithubExplorerApp-TakeHomeProject/ViewModel/SearchResultViewModel.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+protocol SearchViewModelDelegate : AnyObject {
+    func didReceiveUsers()
+}
+class SearchResultViewModel {
+
+    var networkManager = NetworkManager()
+    
+    var delegate : SearchViewModelDelegate?
+    var listOfUser = [User]()
+    var page = 1
+    
+    
+    func requestUsers(_ user : String) {
+        networkManager.requestLogin(user) { users, error in
+            users?.forEach { user in
+                self.listOfUser.append(user)
+            }
+            self.delegate?.didReceiveUsers()
+        }
+    }
+
+    func nextPageUser(_ user : String) {
+        page = page + 1
+        requestUsers(user)
+    }
+}
