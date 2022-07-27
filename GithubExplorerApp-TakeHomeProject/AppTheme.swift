0853d55b68d0f51d08a076156cc40c175b847f3f
Ikmal Azman <opikmalazman@gmail.com>
2022-07-27 19:21:19 +0800

    create app theme for app appearance utilities

---
+++ GithubExplorerApp-TakeHomeProject/AppTheme.swift
@@ -0,0 +1,12 @@
+//
+//  AppTheme.swift
+//  GithubExplorerApp-TakeHomeProject
+//
+//  Created by Ikmal Azman on 27/07/2022.
+//
+
+import UIKit
+
+struct AppTheme {
+    static let primaryGreen = UIColor(named: "primary-green")!
+}
