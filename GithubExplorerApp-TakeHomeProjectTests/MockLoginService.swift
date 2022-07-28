e6a0a3863637284f4880f0e8e599913f5c640729
Ikmal Azman <opikmalazman@gmail.com>
2022-07-28 08:27:10 +0800

    implement basic configuration for mock login service

--- GithubExplorerApp-TakeHomeProjectTests/MockLoginService.swift
+++ GithubExplorerApp-TakeHomeProjectTests/MockLoginService.swift
@@ -10,6 +10,29 @@ import Foundation
 
 class MockLoginService : LoginServiceContract {
     
-    func requestLogin(_ user: String, page: Int, completion: @escaping (Result<[User], APIError>) -> Void) {
+    var requestLoginArguementsUser = [String]()
+    var requestLoginArguementsPage = [Int]()
+    var requestLoginCompletionUser = [User]()
+    
+    var successMakeRequest = false
+    var requestLoginCallCount = 0
+    
+    func requestLogin(_ user: String,
+                      page: Int,
+                      completion: @escaping (Result<[User], APIError>) -> Void) {
+        requestLoginCallCount = requestLoginCallCount + 1
+        requestLoginArguementsUser.append(user)
+        requestLoginArguementsPage.append(page)
+        
+        let response = [
+            User(login: "John", avatar_url: "https:/dummy1.com", type: "User"),
+            User(login: "Doe", avatar_url: "https:/dummy2.com", type: "User"),
+            User(login: "FakeCompany", avatar_url: "https:/dummy2.com", type: "Organization")
+        ]
+        
+        completion(.success(response))
     }
 }
+
+
+#warning("can be scale for network request test")
